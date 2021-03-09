77beec8d968ceb8e8e4f010ce002b5c4fab43c69
Aleksander Aleksiev <aleksander.aleksiev12@abv.bg>
2021-03-09 19:39:33 +0200

    Added transitioning to home page

--- app/components/login.js
+++ app/components/login.js
@@ -1,40 +1,51 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 
 export default class LoginComponent extends Component{
   @tracked email;
   @tracked password;
+  @service router;
+  
+  @action
+  logRequest(){
+    let result=0;
+    let userData = {
+        'email' : this.email,
+        'password' : this.password
+    };
+    let fetchObject = {
+        method: 'POST',
+        headers : {
+            'Content-type' : 'application/json', 
+        },
+        body : JSON.stringify(userData),
+    };
+    fetch('https://gara6.bg/auto-api/users/login',fetchObject)
+        .then(response => {
+            if(!response.ok){
+                throw new Error('Network response was not ok');
+            }
+            result=1;
+        })
+        .catch(error => {
+            alert(`There has been a problem with your fetch operation: ${error}`);
+        });
+    return result;
+  }
+
 
   @action
-  logUser(){
-        let userData = {
-            'email' : this.email,
-            'password' : this.password
-        };
-        let fetchObject = {
-            method: 'POST',
-            headers : {
-                'Content-type' : 'application/json', 
-            },
-            body : JSON.stringify(userData),
-        };
-        fetch('https://gara6.bg/auto-api/users/login',fetchObject)
-            .then(response => {
-                alert(response.status);
-                alert(response.statusText);
-                if(!response.ok){
-                    alert('Network response was not ok');
-                }
-                alert(response.json());
-                return response.json();
-                // alert('Logged In');
-                // this.transitionToRoute('home');
-            })
-            .then(data =>{
-                alert(data);
-            }).catch(error => {
-                alert(`There has been a problem with your fetch operation: ${error}`);
-            });
+  logUser(e){
+    e.preventDefault();
+        let result = this.logRequest;
+        if(result){
+            alert('Logged in');
+            this.router.transitionTo('home');
+        }
+        else{
+            alert('Not logged in');
+        }
     }
 }
